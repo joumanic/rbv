35dad18c8aad279b9816a2cc30469d44542e797a
2406560e <2406560e@student.gla.ac.uk>
2024-09-08 18:01:35 +0100

    gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/data`) 
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
