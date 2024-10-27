2955480bb10e14847b6d66128df87503be08495f
KennethRjp23 <95919958+KennethRjp23@users.noreply.github.com>
2024-10-27 10:36:26 +0000

    Update file_handler.py

--- scripts/create_social_media_assets/file_handler.py
+++ scripts/create_social_media_assets/file_handler.py
@@ -1,18 +1,28 @@
 import os
-from dropbox.services.files import get_folder, download_file, upload_file
+from dropbox.services.files import DropboxService
 
 class FileHandler:
     def __init__(self):
         self.show_images_path = os.getenv("DROPBOX_SHOW_IMAGES")
         self.upload_path = os.getenv("DROPBOX_RBV_SHOW_IMAGES")
+        self.dropbox_service = DropboxService()  # Initialize DropboxService
     
     def get_images(self):
-        folder = get_folder(self.show_images_path)
-        return [file for file in folder["entries"] if file["name"].lower().endswith(("png", "jpg", "jpeg"))]
+        folder = self.dropbox_service.get_folder(self.show_images_path)
+        if folder and "entries" in folder:
+            return [file for file in folder["entries"] if file["name"].lower().endswith(("png", "jpg", "jpeg"))]
+        return []
     
     def file_download(self, file_path):
-        return download_file(file_path = file_path).content
-    
+        response_content = self.dropbox_service.download_file(file_path)
+        if response_content:
+            return response_content  # Return the raw file content
+        return None
+        
     def upload_image(self, img_data, filename):
         upload_path = os.path.join(self.upload_path, filename)
-        upload_file(path=upload_path, data=img_data)
+        response = self.dropbox_service.upload_file(path=upload_path, data=img_data)
+        if response:
+            print("File uploaded successfully.")
+        else:
+            print("Failed to upload the file.")
