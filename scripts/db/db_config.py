8df6e6a333fdd9c5981a5b9c70a4eec7e7700950
KennethRjp23 <95919958+KennethRjp23@users.noreply.github.com>
2024-10-29 22:16:42 +0000

    Update db_config.py

--- scripts/db/db_config.py
+++ scripts/db/db_config.py
@@ -4,6 +4,6 @@ DATABASE_CONFIG = {
     'dbname': os.getenv("DB_NAME"),
     'user': os.getenv("DB_USER"),
     'password': os.getenv("DB_PASSWORD"),
-    'host': os.getenv("DB_HOST"),  # or your database host
-    'port': os.getenv("DB_PORT"),       # default PostgreSQL port
+    'host': os.getenv("DB_HOST"), 
+    'port': os.getenv("DB_PORT"),     
 }
